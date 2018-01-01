a63e45e8452f0f29c925270e6760454f100152c1
David Kingsbury <david@thekingsburys.com>
2018-01-01 02:22:00 -0500

    Reimplement PBKDF2SHA512 using Java 8 Crypto package
    The original PBKDF2SHA512 is a cryptofreek.org implementation of pbkdf2.
    The java doc is copied from the cryptofreek.org post, and states that
    the class passes all the RFC 6070 test vectors. This is not technically
    true for PBKDF2SHA512 because we changed the SecretKeyFactory algorithm
    from HmacSHA1 to HmacSHA512.
    
    The Java 8 implementation of "PBKDF2WithHmacSHA512" is used to satisfy
    the original method signature for derive(...).

--- core/src/main/java/org/bitcoinj/crypto/PBKDF2SHA512.java
+++ core/src/main/java/org/bitcoinj/crypto/PBKDF2SHA512.java
@@ -1,112 +1,46 @@
-/*
- * Copyright (c) 2012 Cole Barnes [cryptofreek{at}gmail{dot}com]
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- */
-
 package org.bitcoinj.crypto;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 /**
- * <p>This is a clean-room implementation of PBKDF2 using RFC 2898 as a reference.</p>
- *
- * <p>RFC 2898: http://tools.ietf.org/html/rfc2898#section-5.2</p>
- *
- * <p>This code passes all RFC 6070 test vectors: http://tools.ietf.org/html/rfc6070</p>
- *
- * <p>http://cryptofreek.org/2012/11/29/pbkdf2-pure-java-implementation/<br>
- * Modified to use SHA-512 - Ken Sedgwick ken@bonsai.com</p>
+ * This class provides PBKDF2 with SHA-512 HMAC key derivation using
+ * {@link PBEKeySpec} and {@link SecretKeyFactory} of the Java Crypto package.
  */
 public class PBKDF2SHA512 {
-    public static byte[] derive(String P, String S, int c, int dkLen) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
+    /**
+     * Derive a secret key from a password using PBKDF2 with SHA-512 HMAC
+     * @param password material from which the cryptographic key is derived
+     * @param salt the salt
+     * @param iterationCount the iteration count
+     * @param keyLength the to-be-derived key length in bytes
+     * @return the secret key
+     */
+    public static byte[] derive(String password, String salt, int iterationCount, int keyLength) {
+        /*
+         * Encode salt bytes to UTF-8 as specified in BIP39
+         */
+        final byte[] saltBytes;
         try {
-            int hLen = 20;
-
-            if (dkLen > ((Math.pow(2, 32)) - 1) * hLen) {
-                throw new IllegalArgumentException("derived key too long");
-            } else {
-                int l = (int) Math.ceil((double) dkLen / (double) hLen);
-                // int r = dkLen - (l-1)*hLen;
-
-                for (int i = 1; i <= l; i++) {
-                    byte[] T = F(P, S, c, i);
-                    baos.write(T);
-                }
-            }
-        } catch (Exception e) {
+            saltBytes = salt.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
-
-        byte[] baDerived = new byte[dkLen];
-        System.arraycopy(baos.toByteArray(), 0, baDerived, 0, baDerived.length);
-
-        return baDerived;
-    }
-
-    private static byte[] F(String P, String S, int c, int i) throws Exception {
-        byte[] U_LAST = null;
-        byte[] U_XOR = null;
-
-        SecretKeySpec key = new SecretKeySpec(P.getBytes("UTF-8"), "HmacSHA512");
-        Mac mac = Mac.getInstance(key.getAlgorithm());
-        mac.init(key);
-
-        for (int j = 0; j < c; j++) {
-            if (j == 0) {
-                byte[] baS = S.getBytes("UTF-8");
-                byte[] baI = INT(i);
-                byte[] baU = new byte[baS.length + baI.length];
-
-                System.arraycopy(baS, 0, baU, 0, baS.length);
-                System.arraycopy(baI, 0, baU, baS.length, baI.length);
-
-                U_XOR = mac.doFinal(baU);
-                U_LAST = U_XOR;
-                mac.reset();
-            } else {
-                byte[] baU = mac.doFinal(U_LAST);
-                mac.reset();
-
-                for (int k = 0; k < U_XOR.length; k++) {
-                    U_XOR[k] = (byte) (U_XOR[k] ^ baU[k]);
-                }
-
-                U_LAST = baU;
-            }
+        /*
+         * A previous implementation of this method used bytes for the key length. Convert to bits
+         * for PBEKeySpec.
+         */
+        final int keyLengthBits = keyLength * 8;
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, iterationCount, keyLengthBits);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
         }
-
-        return U_XOR;
-    }
-
-    private static byte[] INT(int i) {
-        ByteBuffer bb = ByteBuffer.allocate(4);
-        bb.order(ByteOrder.BIG_ENDIAN);
-        bb.putInt(i);
-
-        return bb.array();
     }
 }
